fedb57e7230ada531f895188427fbe07679edeb7
ninahg <nina.hang@outlook.com>
2026-02-12 11:35:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOneEl = document.getElementById("passwordOne")
+let passwordTwoEl = document.getElementById("passwordTwo")
+
+function generatePasswords() {
+    let passwordOne = ""
+    let passwordTwo = ""
+    
+for (let i = 0; i < 15; i++) {
+    let randomIndexOne = Math.floor( Math.random() * characters.length )
+    let randomIndexTwo = Math.floor( Math.random() * characters.length )
+    passwordOne += characters[randomIndexOne]
+    passwordTwo += characters[randomIndexTwo]
+}
+    passwordOneEl.textContent = passwordOne
+    passwordTwoEl.textContent = passwordTwo
+}
